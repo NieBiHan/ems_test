c8e7ea9b903a944953f50cb8f2a341987e7adb57
NieBiHan <756895275@qq.com>
2019-09-25 15:53:57 +0800

    这是第一次提交的项目

---
+++ src/test/java/com/baizhi/ems_test/EmsTestApplicationTests.java
@@ -0,0 +1,18 @@
+package com.baizhi.ems_test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmsTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+        System.out.println("1111111111111111111");
+    }
+
+}
